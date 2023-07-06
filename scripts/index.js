e7d39a7b1e6d025b6130ad148b36c24d44c0f67c
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-07-07 00:32:05 +0500

    Добавлена основная структура приложения

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+// Ссылка на поле ввода фильмов 
+const movieInputNode = document.querySelector('#movieInput');
+// Ссылка на кнопку добавления фильмов из поля ввода
+const newMovieButtonNode = document.querySelector('#newMovieButton');
+
